e8cf715c344df2c94ae023433a40c218236772c6
Niko-PL <146205724+Niko-PL@users.noreply.github.com>
2023-10-05 15:40:06 +0100

    Lesson Practice on 05/10/23

---
+++ Lesson Practice.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+
+/*  
+int main()              //this is task 203
+{
+    puts("Press a letter or number, then press return");
+    puts("Press q then return to quit");
+
+    char userEnteredCharacter;
+    char returnKey;
+
+    do
+    {
+        userEnteredCharacter = getchar();   //ASCII character
+        returnKey = getchar();              //Return key
+        printf("You typed %c\n", userEnteredCharacter);
+        printf("You also pressed return (ASCII %d)\n", returnKey); //Uncomment this
+        cout << "HERE";
+        cout << userEnteredCharacter;
+
+        //Use either of the following lines (but not both)
+    } while ((userEnteredCharacter != 'q') && (userEnteredCharacter != 'Q'));            //Repeat if condition is met - note the != operator 
+
+    puts("Finished");
+}
+
+*/
+
+/*
+void main(void)
+{
+    int Temp;
+    Temp = 10;
+    
+    do
+    {
+        printf("This is Temp %d\n", Temp);
+        
+    } while (Temp >= 0; Temp -= 2);
+}
+*/
+
+/*
+void main(void)
+{
+    for (int x = 0; x <= 10; x++)
+    {
+        printf("THIS IS X%d\n", x);
+    }
+}
+*/
