910ff8dbe0addac20b67881b9f2e288306ae27d1
Niko-PL <nikus1ilula@gmail.com>
2023-10-25 18:20:30 +0100

    opening and closing files practice
    did questions regarding the programing tasks

--- Tasks/06 - FileIO/04-TimesTables/04-TimesTables.cpp
+++ Tasks/06 - FileIO/04-TimesTables/04-TimesTables.cpp
@@ -5,13 +5,34 @@ using namespace std;
 
 int main()
 {
-	for (unsigned int r = 2; r <= 12; r++) {
-		for (unsigned int c = 2; c <= 12; c++) {
-			cout << r * c << "\t";
+	ofstream outputStream;
+	outputStream.open("Multiply.txt");			//open or create the file
+
+	if (outputStream.is_open() == false)			//did it open if not enter if
+	{
+		cout << "ERROR with FILE" << endl;
+		return -1;		//-1 will end the program :)
+	}
+
+	for (unsigned int r = 2; r <= 12; r++) 
+	{
+		for (unsigned int c = 2; c <= 12; c++) 
+		{
+			int math = r * c;
+
+			cout << math << "\t";
+			
+			outputStream << math << "\t";		//save to the file row by row.
+			
 		}
 		cout << endl;
+		outputStream << endl;
 	}
 	cout << endl;
+	outputStream << endl;
+
+	outputStream.close();		//close the file
 
 	return 0;
 }
+
